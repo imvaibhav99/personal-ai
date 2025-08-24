ac1c9468737f2ee83bae739941e025b90ad488a5
imvaibhav99 <imvaibhav9796@gmail.com>
2025-08-25 00:43:45 +0530

    multer

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,5 +1,21 @@
+// import multer from "multer";
+
+// const storage= multer.diskStorage({});
+
+// export const upload= multer({storage})
+
+
 import multer from "multer";
+import { CloudinaryStorage } from "@fluidjs/multer-cloudinary";
+import cloudinary from "./cloudinary.js";
 
-const storage= multer.diskStorage({});
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads", // Cloudinary folder name
+    allowed_formats: ["jpg", "jpeg", "png", "pdf", "doc", "docx"],
+    resource_type: "auto",
+  },
+});
 
-export const upload= multer({storage})
+export const upload = multer({ storage });
